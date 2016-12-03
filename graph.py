712b1087788b33a197c4e020106154e46a127978
dhruven <dhruven91@gmail.com>
2016-12-02 23:45:21 -0800

    added data files and graph class

---
+++ graph.py
@@ -0,0 +1,29 @@
+class Graph:
+    
+    __init__(self):
+        #Storing weights as matrix
+        self.usersCount = 0
+        self.actorsCount = 0
+        self.directorsCount = 0
+        self.genreCount = 0
+        self.affinity_AD = []
+        self.affinity_GD = []
+        self.affinity_AG = []
+        
+    _add_count(self, usersCount, actorsCount, directorsCount, genreCount):
+        self.usersCount = usersCount
+        self.actorsCount = actorsCount
+        self.directorsCount = directorsCount
+        self.genreCount = genreCount
+        
+    _add_matrix(self, movies,             
+    
+    _calculate_affinity_between_AD(self):
+        
+        
+    _calculate_affinity_between_GD(self):
+        
+        
+    _calculate_affinity_between_AG(self):
+        
+    
