5642df10f690e4963ddc5882f681257727ebaed8
tapomay dey <tadey@cisco.com>
2016-12-03 02:33:07 -0800

    Removing model. Updated pwm code.

--- pwm.py
+++ pwm.py
@@ -6,14 +6,15 @@ MAX_COUNT = 1000000
 class PositionWeightMatrix:
 
     def __init__(self):
+        self._pwmFreqs = {}
         self._pwm = {}
         # self._indices = {}
         self._newIndex = 0
 
     def addFactRelation(self, e1, e2, weight=None, increment=False):
-        if e1 not in self._pwm:
-            self._pwm[e1] = {}
-        dim1 = self._pwm[e1]
+        if e1 not in self._pwmFreqs:
+            self._pwmFreqs[e1] = {}
+        dim1 = self._pwmFreqs[e1]
 
         if e2 not in dim1:
             dim1[e2] = 0
@@ -44,8 +45,25 @@ class PositionWeightMatrix:
     def parseLike(self, fObj):
         fromObj = fObj['userID']
         toObj = fObj['movieID']
-        weight = fObj['rating']
+        weight = float(fObj['rating'])
         return (fromObj, toObj, weight)
+
+    def finalizePWM(self):
+        for dim1, dim2Dict in self._pwmFreqs.items():
+            sumWt = 0
+            for dim2,wt in dim2Dict.items():
+                sumWt += wt
+            for dim2,wt in dim2Dict.items():
+                newWt = wt / float(sumWt)
+                if dim1 not in self._pwm:
+                    self._pwm[dim1] = {}
+                self._pwm[dim1][dim2] = newWt
+            
+        for dim1, dim2Dict in self._pwm.items():
+            sumWt = 0
+            for dim2,wt in dim2Dict.items():
+                sumWt += wt
+            print("DIM-%s : %f" % (dim1, sumWt))
 
 def main(jsonFiles):
     pwm = PositionWeightMatrix()
@@ -53,7 +71,8 @@ def main(jsonFiles):
         with open(fname, 'r') as f:
             fObjArr = json.loads(f.read())
             pwm.addDBObjArr(fObjArr, pwm.parseLike)
-    print(len(pwm._pwm))
+    print(len(pwm._pwmFreqs))
+    pwm.finalizePWM()
     
     s = pickle.dumps(pwm)
     with open('model', 'w') as f:
