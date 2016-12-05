5b5416cc4edac6615e94de411bbd388e28c5f0a2
tapomay dey <tadey@cisco.com>
2016-12-04 23:18:20 -0800

    Adding likes parser. Removing pwm

---
+++ likes_parser.py
@@ -0,0 +1,93 @@
+import json
+import pickle
+import time
+
+PICKLE_FILE = 'likes.model'
+
+class UserMovieRel:
+
+    def __init__(self, userId, movieId, rating):
+        self._movie = movieId
+        self._user = userId
+        self._rating = float(rating)
+
+    @property
+    def user(self):
+        return self._user
+    
+    @property
+    def movie(self):
+        return self._movie
+    
+    @property
+    def rating(self):
+        return self._rating
+    
+class LikesParser:
+
+    def __init__(self, likeThreshold = 3):
+        self._likeThreshold = likeThreshold
+    
+    def _parseFile(self, filepath, count = None):
+        with open(filepath, 'r') as f:
+            fileStr = f.read()
+            fileJsonArr = json.loads(fileStr)
+            idx = 0
+            for jsonObj in fileJsonArr:
+                userMovieRel = UserMovieRel(jsonObj['userID'], jsonObj['movieID'], jsonObj['rating'])
+                yield userMovieRel
+                idx += 1
+                if count:
+                    if idx >= count:
+                        break
+            print("Parsed %d likes" % idx)
+
+
+    def _buildUserMoviesDict(self, filepath, count = None):
+        retDict = {}
+        for userMovieRel in self._parseFile(filepath, count):
+            if userMovieRel.user not in retDict:
+                retDict[userMovieRel.user] = []
+            retDict[userMovieRel.user].append(userMovieRel)
+        return retDict
+
+    def _buildUserLikesTupleDict(self, userMoviesDict):
+        for user, moviesList in userMoviesDict.items():
+            watchedSet = set()
+            likedSet = set()
+            for userMovieRel in moviesList:
+                if userMovieRel.rating > self._likeThreshold:
+                    likedSet.add(userMovieRel.movie)
+                watchedSet.add(userMovieRel.movie)
+            yield (user, watchedSet, likedSet)
+
+    def getUserDict(self, filepath, build = False, count = None):
+        startTime = time.time()
+        if not build:
+            with open(PICKLE_FILE, 'r') as f:
+                s = f.read()
+                model = pickle.loads(s)
+            print("Model loaded from pickle in %f seconds" % (time.time() - startTime))
+        else:
+            model = {}
+            userMoviesDict = self._buildUserMoviesDict(filepath, count)
+            # print(userMoviesDict)
+            for (user, watchedSet, likedSet) in self._buildUserLikesTupleDict(userMoviesDict):
+                model[user] = (watchedSet, likedSet)
+            print("Parser completed in %f seconds" % (time.time() - startTime))
+            s = pickle.dumps(model)
+            with open(PICKLE_FILE, 'w') as f:
+                f.write(s)
+        return model
+
+def main():
+    filepath = 'json/likes.json'
+    parser = LikesParser(3)
+    userDict = parser.getUserDict(filepath, build = False, count = None)
+    likeProbs = []
+    for user, items in userDict.items():
+        print("%s: %d->%d" % (user, len(items[0]), len(items[1])))
+        likeProbs.append(len(items[1]) / float(len(items[0])))
+    print("Average Like prob: %f" % (sum(likeProbs) /float(len(likeProbs))))
+if __name__ == "__main__":
+    main()
