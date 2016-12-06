76806cb586216f53412cdc277d56eb2449e7b2b6
tapomay dey <tadey@cisco.com>
2016-12-05 18:25:15 -0800

    Updated movies parser. Added pickling

--- moviesParser.py
+++ moviesParser.py
@@ -1,51 +1,74 @@
 import json
+import pickle
+import time
+
+MOVIES_PICKLE_FILE = 'movies.model'
 
 class Movies:
-    dictDirector = {}
-    dictActor = {}
-    dictGenre = {}
+
     def __init__(self):
-        self.movieID = 0
+        self.dictDirector = {} # {director : [movieID]}
+        self.dictActor = {} # {actor : [movieID]}
+        self.dictGenre = {} # {genre : [movieID]}
     
-    def returnMovieID(self, objFromJson):                
-         self.movieID = objFromJson['movie_id'] 
-         return self.movieID                           
+    def returnMovieID(self, movieObject):
+         return movieObject['movie_id']
 
     #method that checks if ID of dict contains directors: if not, create one dict with dir as ID and movieID as value                                
-    def dictOfDirectors(self, objFromJson, movieID, dictDirector):
-        director = objFromJson['director']       
-        if director not in self.dictDirector.keys():
+    def updateDictOfDirectors(self, movieObject):
+        director = movieObject['director']
+        if director not in self.dictDirector:
             self.dictDirector[director] = set()
-        self.dictDirector[director].add(self.movieID)    
+        self.dictDirector[director].add(self.returnMovieID(movieObject))
         
     #method that checks if ID of dict contains actors: if not, create one dict with actor as ID and set of movieIDs as value    
-    def dictOfActors(self, objFromJson, movieID, dictActor):
-        listOfActors = objFromJson['actors']        
-        for actor in listOfActors:       
-            if actor not in self.dictActor.keys():
-                self.dictActor[actor] = set() 
-            self.dictActor[actor].add(self.movieID)    
+    def updateDictOfActors(self, movieObject):
+        listOfActors = movieObject['actors']
+        for actor in listOfActors:
+            if actor not in self.dictActor:
+                self.dictActor[actor] = set()
+            self.dictActor[actor].add(self.returnMovieID(movieObject))
         
     #method that checks if ID of dict contains genres: if not, create one dict with genre as ID and set of movieIDs as value        
-    def dictOfGeneres(self, objFromJson, movieID, dictGenre):      
-        genre = objFromJson['genre']     
-        if genre not in self.dictGenre.keys():
+    def updateDictOfGeneres(self, movieObject):
+        genre = movieObject['genre']
+        if genre not in self.dictGenre:
             self.dictGenre[genre] = set()  
-        self.dictGenre[genre].add(self.movieID)   
-        
+        self.dictGenre[genre].add(self.returnMovieID(movieObject))
 
-#main function to parse a json file of movies:                
+    def parseMoviesObjects(self, filename,  parse = True):
+        start_time = time.time()
+        if parse:
+            json_data =  open(moviesJson, 'r').read() 
+            jsonData = json.loads(json_data) 
+                                  
+            for movieObject in jsonData: 
+                self.updateDictOfDirectors(movieObject)
+                self.updateDictOfActors(movieObject)
+                self.updateDictOfGeneres(movieObject)
+            print("Parsed movies.json in %f seconds" % (time.time() - start_time))
+            s = pickle.dumps(self)
+            with open(MOVIES_PICKLE_FILE, 'w') as f:
+                f.write(s)
+        else:
+            with open(MOVIES_PICKLE_FILE, 'r') as f:
+                s = f.read()
+                mov = pickle.loads(s)
+                self.dictActor = mov.dictActor
+                self.dictDirector = mov.dictDirector
+                self.dictGenre = mov.dictDirector
+            print("Loaded pickled movies model in %f seconds" % (time.time() - start_time))
+        return self
+
+
+#main function to parse a json file of movies:
 def main(moviesJson):  
-    json_data =  open(moviesJson, 'r').read() 
-    jsonData = json.loads(json_data) 
-    
     mov = Movies()
-                  
-    for movieObject in jsonData: 
-        mov.dictOfDirectors(movieObject, mov.returnMovieID, mov.dictDirector)
-        mov.dictOfActors(movieObject, mov.returnMovieID, mov.dictActor)
-        mov.dictOfGeneres(movieObject, mov.returnMovieID, mov.dictGenre)
-                        
+    mov.parseMoviesObjects(moviesJson, parse = False)
+    print(len(mov.dictActor))
+    print(len(mov.dictDirector))
+    print(len(mov.dictGenre))
+
 if __name__ == "__main__":
-    moviesJson = 'movies.json'
+    moviesJson = 'json/movies.json'
     main(moviesJson)
